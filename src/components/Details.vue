c9e6190f6e0fb7e13f71e2e62a16b0e3dfa4f11e
Brett Wilcox <wilcox.brett@gmail.com>
2019-03-05 17:38:31 -0700

    17:39

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,42 +1,58 @@
 <template>
   <div class="details col-5 offset-1">
-    <img class="mt-5" :src="song.artworkUrl100" alt="">
+    <img class="mt-5" :src="song.artworkUrl100" alt>
     <h3 class="text-white mt-3">{{song.trackName}} by {{song.artistName}}</h3>
     <h4 class="text-white">Album: {{song.collectionName}}</h4>
     <h4 class="text-white" :released="released">Released: {{song.releaseDate}}</h4>
-    <a class="text-white btn btn-sm btn-outline-light mt-2" :href="song.trackViewUrl" target="_blank">Download Track
-      ${{song.trackPrice}} </a>
-    <a class="text-white btn btn-sm btn-outline-light ml-3 mt-2" :href="song.collectionViewUrl" target="_blank">Download
+    <a
+      class="text-white btn btn-sm btn-outline-light mt-2"
+      :href="song.trackViewUrl"
+      target="_blank"
+    >
+      Download Track
+      ${{song.trackPrice}}
+    </a>
+    <a
+      class="text-white btn btn-sm btn-outline-light ml-3 mt-2"
+      :href="song.collectionViewUrl"
+      target="_blank"
+    >
+      Download
       Album
-      ${{song.collectionPrice}} </a>
-    <audio controls class="my-audio ml-5 song-audio mt-4" id="playAudio" :src="song.previewUrl" type="audio/ogg">
-    </audio>
+      ${{song.collectionPrice}}
+    </a>
+    <audio
+      controls
+      class="my-audio ml-5 song-audio mt-4"
+      id="playAudio"
+      :src="song.previewUrl"
+      type="audio/ogg"
+    ></audio>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Details',
-    props: ['song'],
-    data() {
-      return {
-        released: ''
-      }
-    },
-    computed: {},
-    methods: {
-    }
-  }
+export default {
+  name: "Details",
+  props: ["song"],
+  data() {
+    return {
+      released: ""
+    };
+  },
+  computed: {},
+  methods: {}
+};
 </script>
 
 <style>
-  .text-white {
-    text-shadow: 2px 1px black;
-  }
+.text-white {
+  text-shadow: 2px 1px black;
+}
 
-  img {
-    height: 20vh;
-    width: auto;
-    margin-left: 8vw;
-  }
+img {
+  height: 20vh;
+  width: auto;
+  margin-left: 8vw;
+}
 </style>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -3,62 +3,62 @@
     <div class="col-6" id="song-results">
       <h2 class="text-white ml-2 mt-4">{{artistName}} results:</h2>
       <ul>
-        <li class="text-white" v-for="song in results" @click='setActiveSong(song)'>
+        <li class="text-white" v-for="song in results" @click="setActiveSong(song)">
           {{song.trackName}} :
-          {{song.collectionName}}</li>
+          {{song.collectionName}}
+        </li>
       </ul>
     </div>
-    <details-view :song='activeSong' v-if="activeSong.trackName"></details-view>
+    <details-view :song="activeSong" v-if="activeSong.trackName"></details-view>
   </div>
 </template>
 
 <script>
-  import DetailsView from '@/components/Details.vue'
-  export default {
-    name: '',
-    props: ['artistName'],
-    data() {
-      return {
-        activeSong: {}
-      }
-    },
-    computed: {
-      results() {
-        let songs = this.$store.state.results
-        return songs
-      }
-    },
-    methods: {
-      setActiveSong(song) {
-        this.activeSong = song
-      }
-    },
-    components: {
-      DetailsView
+import DetailsView from "@/components/Details.vue";
+export default {
+  name: "",
+  props: ["artistName"],
+  data() {
+    return {
+      activeSong: {}
+    };
+  },
+  computed: {
+    results() {
+      let songs = this.$store.state.results;
+      return songs;
     }
+  },
+  methods: {
+    setActiveSong(song) {
+      this.activeSong = song;
+    }
+  },
+  components: {
+    DetailsView
   }
-
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
+h3 {
+  margin: 40px 0 0;
+}
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-  li {
-    display: inline-block;
-    margin: 0 10px;
-    text-shadow: 2px 1px black;
-    cursor: pointer;
-  }
+li {
+  display: inline-block;
+  margin: 0 10px;
+  text-shadow: 2px 1px black;
+  cursor: pointer;
+}
 
-  a {
-    color: black;
-  }
+a {
+  color: black;
+}
 </style>
